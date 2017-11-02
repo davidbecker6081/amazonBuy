23a05e7255259e523b36b2b580476269751c5220
David Becker <david@beckersite.com>
2017-11-02 13:23:49 -0600

    Add skeleton for order-history

--- public/script.js
+++ public/script.js
@@ -1,26 +1,20 @@
-const appendItems = (items) => {
-  items.forEach(item => {
-    $('main').append(`
-      <article class="item-card">
+const appendItems = items => {
+	items.forEach(item => {
+		$('.items-container').append(
+      `<article class="item-card">
         <h3>${item.item}</h3>
         <p><strong>Description: </strong>${item.item_description}</p>
         <img src=${item.item_url} alt="bass guitar beauty">
         <p><strong>Price: </strong>$${item.price}.00</p>
         <button data-id=${item.id} class="add-cart-btn">Add To Cart</button>
-      </article>
-      `)
-  })
-
-  }
-
+      </article>`);
+	});
+};
 
 const getInventory = () => {
-  fetch('/api/v1/inventory')
-    .then(response => response.json())
-    .then(parsedResponse => appendItems(parsedResponse))
-}
+	fetch('/api/v1/inventory')
+		.then(response => response.json())
+		.then(parsedResponse => appendItems(parsedResponse));
+};
 
-
-
-
-$(document).ready(getInventory)
+$(document).ready(getInventory);
