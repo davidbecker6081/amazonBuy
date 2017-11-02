bc516653e1318f6b284eb29d153706ea6513e49b
David Becker <david@beckersite.com>
2017-11-02 09:35:14 -0600

    Finish folder file skeleton

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
