2f00b07a9ee70bb1dbb84baa27b3e65ebb5c389f
David Becker <david@beckersite.com>
2017-11-02 12:29:04 -0600

    Add sad path testing for post to order history

--- db/seeds/dev/inventory.js
+++ db/seeds/dev/inventory.js
@@ -50,17 +50,6 @@ exports.seed = function(knex, Promise) {
 			]);
 		})
 		.then(() => {
-			return knex('order_history')
-				.del()
-				.then(() => {
-					return knex('inventory').insert([
-						{ id: 1, total: 1000 },
-						{ id: 2, total: 2000 },
-						{ id: 3, total: 3000 },
-					]);
-				});
-		})
-		.then(() => {
 			console.log('Seeding is complete.');
 		})
 		.catch(error => console.log(`Error seeding data: ${error}`));
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -40,7 +40,7 @@ describe('Client Routes', () => {
 });
 
 describe('API routes', () => {
-	beforeEach(done => {
+	before(done => {
 		db.migrate
 			.rollback()
 			.then(() => db.migrate.latest())
@@ -117,7 +117,7 @@ describe('API routes', () => {
 					response.body[0].should.have.property('id');
 					response.body[0].id.should.be.a('number');
 					response.body[0].should.have.property('total');
-          response.body[0].total.should.be.a('number');
+					response.body[0].total.should.be.a('number');
 					response.body[0].should.have.property('created_at');
 					response.body[0].created_at.should.be.a('string');
 					response.body[0].should.have.property('updated_at');
@@ -135,5 +135,96 @@ describe('API routes', () => {
 				response.should.have.status(404);
 				done();
 			});
+	});
+
+	describe('POST /api/v1/order_history', () => {
+		it('should add an order to the order history', done => {
+			chai
+				.request(server)
+				.get('/api/v1/order_history')
+				.end((error, response) => {
+					response.should.have.status(200);
+					response.body.length.should.equal(3);
+
+					chai
+						.request(server)
+						.post('/api/v1/order_history')
+						.send({
+							id: 4,
+							total: 9999,
+						})
+						.end((error, response) => {
+							response.should.have.status(201);
+							response.body[0].id.should.equal(4);
+							response.body[0].total.should.equal(9999);
+
+							chai
+								.request(server)
+								.get('/api/v1/order_history')
+								.end((error, response) => {
+									response.should.have.status(200);
+									response.body.length.should.equal(4);
+									done();
+								});
+						});
+				});
+		});
+
+		it('should not add a new order if missing a required key', done => {
+			chai
+				.request(server)
+				.get('/api/v1/order_history')
+				.end((error, response) => {
+					response.should.have.status(200);
+					response.body.length.should.equal(3);
+
+					chai
+						.request(server)
+						.post('/api/v1/order_history')
+						.send({
+							id: 4,
+						})
+						.end((error, response) => {
+							response.should.have.status(422);
+							response.should.be.json;
+							response.body.error.should.equal(
+								"Expected format: {'total': <decimal>}. You are missing a total property."
+							);
+						});
+
+					chai
+						.request(server)
+						.get('/api/v1/order_history')
+						.end((error, response) => {
+							response.should.have.status(200);
+							response.body.length.should.equal(3);
+							done();
+						});
+				});
+		});
+		//
+		// 	it('should return an error message if request body is missing required keys', done => {
+		// 		chai
+		// 			.request(server)
+		// 			.post('/api/v1/breweries')
+		// 			.set('Authorization', adminToken)
+		// 			.send({
+		// 				name: 'Tyler\'s Beer Kingdom',
+		// 				location: 'Port Charlotte',
+		// 				year: 1993,
+		// 				email: 'tyler@turing.io',
+		// 				appName: 'Awesome',
+		// 				token: adminToken.token,
+		// 				admin: adminToken.admin,
+		// 			})
+		// 			.end((error, response) => {
+		// 				response.should.have.status(400);
+		// 				response.should.be.json;
+		// 				response.body.error.should.equal(
+		// 					'Missing key: beerCount'
+		// 				);
+		// 				done();
+		// 			});
+		// 	});
 	});
 });
