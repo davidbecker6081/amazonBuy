54667e2d74f0d51d9b5af20e7fea3befb28f2b82
David Becker <david@beckersite.com>
2017-11-02 13:14:58 -0600

    Add appendItems and get Inventory

--- public/script.js
+++ public/script.js
@@ -1,11 +1,25 @@
+const appendItems = (items) => {
+  items.forEach(item => {
+    $('main').append(`
+      <article class="item-card">
+        <h3>${item.item}</h3>
+        <p><strong>Description: </strong>${item.item_description}</p>
+        <img src=${item.item_url} alt="bass guitar beauty">
+        <p><strong>Price: </strong>$${item.price}.00</p>
+        <button data-id=${item.id} class="add-cart-btn">Add To Cart</button>
+      </article>
+      `)
+  })
+
+  }
+
+
 const getInventory = () => {
-  fetch('/')
+  fetch('/api/v1/inventory')
+    .then(response => response.json())
+    .then(parsedResponse => appendItems(parsedResponse))
 }
 
-const appendItem = (item) => {
-  $('main').append(`
-    `)
-}
 
 
 
