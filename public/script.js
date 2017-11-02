d610f50293e1cda17f9f5447869769e6a08d05ce
David Becker <david@beckersite.com>
2017-11-02 14:45:42 -0600

    Add totaling functionality

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ const appendItems = items => {
         <p><strong>Description: </strong>${item.item_description}</p>
         <img src=${item.item_url} alt="bass guitar beauty">
         <p><strong>Price: </strong>$${item.price}.00</p>
-        <button data-item=${item} class="add-cart-btn">Add To Cart</button>
+        <button data-name='${item.item}' data-price=${item.price} class="add-cart-btn">Add To Cart</button>
       </article>`);
 	});
 };
@@ -16,6 +16,30 @@ const getInventory = () => {
 		.then(response => response.json())
 		.then(parsedResponse => appendItems(parsedResponse));
 };
+
+const appendToCart = (name, price) => {
+  $('.cart-article-container').append(
+    `<article class="cart-article">
+      <h4>${name}</h4>
+      <p data-price=${price} class="cart-item-price">Price: $${price}.00</p>
+    </article>`)
+}
+
+const updateTotalCartPrice = () => {
+  let total = 0;
+  $('.cart-item-price').each((i, price) => {
+    total += $(price).data('price')
+  })
+  console.log(total);
+  $('.cart-total').text(total)
+}
+
+$('.items-container').on('click', '.add-cart-btn', (e) => {
+  const name = $(e.target).data('name');
+  const price = $(e.target).data('price')
+  appendToCart(name, price)
+  updateTotalCartPrice()
+})
 
 $('.slide-order-btn').on('click', () => {
   $('.order-history').toggleClass('isActive');
