fbcbce8197cb10f8b391d0fe10240b2991c15720
David Becker <david@beckersite.com>
2017-11-02 09:54:19 -0600

    Add knex migration file

---
+++ db/migrations/20171102094005_initial.js
@@ -0,0 +1,27 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable("inventory", table => {
+      table.increments("id").primary();
+      table.string("item");
+      table.decimal("price").type(1.00);
+      table.string("item_description");
+      table.string("item_url");
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable("order_history", table => {
+      table.increments("id").primary();
+      table.decimal("total").type(1.00);
+
+      table.timestamps(true, true);
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable("order_history"),
+    knex.schema.dropTable("inventory")
+  ]);
+};
