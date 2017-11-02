fc0b058573dd702465c7ec0e1656c70e79280e92
David Becker <david@beckersite.com>
2017-11-02 11:30:34 -0600

    Add seed files for testing environment

--- db/seeds/test/inventory-test.js
+++ db/seeds/test/inventory-test.js
@@ -50,17 +50,6 @@ exports.seed = function(knex, Promise) {
 			]);
 		})
 		.then(() => {
-			return knex('order_history')
-				.del()
-				.then(() => {
-					return knex('inventory').insert([
-						{ id: 1, total: 1000.00 },
-						{ id: 2, total: 2000.00 },
-						{ id: 3, total: 3000.00 },
-					]);
-				});
-		})
-		.then(() => {
 			console.log('Seeding is complete.');
 		})
 		.catch(error => console.log(`Error seeding data: ${error}`));
