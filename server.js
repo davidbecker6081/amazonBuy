6205f7a97435e5fda25d2d4a1da81504576a9b5f
David Becker <david@beckersite.com>
2017-11-02 11:06:15 -0600

    Add post to inventory

--- server.js
+++ server.js
@@ -30,4 +30,39 @@ app.get('/api/v1/inventory', (request, response) => {
 		.catch(error => response.status(500).json({ error }));
 });
 
+app.post('/api/v1/inventory', (request, response) => {
+  const item = request.body;
+
+  const requiredKeys = ['item', 'price', 'item_description', 'item_url'];
+
+  for (const requiredParameter of requiredKeys) {
+    if (!item[requiredParameter]) {
+      console.log(item);
+      return response.status(422).send({
+        error: `Expected format: {'item': <string>, 'price': <decimal>, 'item_description': <string>, 'item_url': <string>}. You are missing a ${requiredParameter} property.`
+      });
+    };
+  };
+
+  return db('inventory')
+    .insert(item, '*')
+    .then(postedItem => response.status(201).json(postedItem))
+    .catch(error => response.status(500).json({ error }));
+})
+
+// app.delete('/api/v1/inventory')
+
+app.get('/api/v1/order_history', (request, response) => {
+  return db('order_history')
+    .select()
+    .then(history => {
+      if (!history.length) {
+        return response.status(404).json({ error: 'No order history could be found.' });
+      } else {
+        return response.status(200).json(history);
+      }
+    })
+    .catch(error => response.status(500).json({ error }));
+})
+
 module.exports = app;
